1e781c1df72179069828f4776f31f211265ea2b7
jrcomey <jack.rhys.comey@gmail.com>
2026-05-15 15:21:25 -0700

    util func file, added load ply

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate brush_render;
+extern crate anyhow;
 use brush_render::camera::Camera;
+mod util;
+
 
 fn main() {
     println!("Hello, world!");
---
+++ src/util.rs
@@ -0,0 +1,10 @@
+use brush_render::{*, gaussian_splats::Splats};
+use anyhow;
+type Backend = brush_render::MainBackend;
+
+
+fn load_ply_file(filepath: std::path::PathBuf) -> anyhow::Result<Backend> {
+
+
+    todo!()
+}
