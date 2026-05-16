900117a58f284eda56d6153e7e110740f0e5545f
jrcomey <jack.rhys.comey@gmail.com>
2026-05-15 18:50:02 -0700

    Added start of a render function

--- src/util.rs
+++ src/util.rs
@@ -27,25 +27,40 @@ pub async fn load_ply_file(filepath: impl AsRef<Path>, subsample_points: Option<
 
 
 
-pub fn render(request: message::ImageRequest, splats: Splats) {
-
-    // let camera = brush_render::camera::Camera::new(
-    //     position,           // Position 
-    //     rotation,           // Rotation
-    //     fov_x,              // FOV in x
-    //     fov_y,              // FOV in y
-    //     center_uv           // FIXME
-    // )
-
-    // gaussian_splats::render_splats(
-    //     splats, 
-    //     camera, 
-    //     img_size, 
-    //     background, 
-    //     splat_scale, 
-    //     texture_mode
-    // );
+pub async fn render(request: message::ImageRequest, splats: Splats) {
     
+    let position = glam::Vec3 { x: 10.0, y: 10.0, z: 10.0 };
+    let rotation = glam_quat([1.0, 0.0, 0.0, 0.0]);
+    let center_uv = glam::Vec2::new(100.0, 100.0);
+    let img_size = glam::UVec2 { x: 100, y: 100 };
+    let background = glam::Vec3 { x: 0.0, y: 0.0, z: 0.0 };
+
+    let camera = brush_render::camera::Camera::new(
+        position,           // Position 
+        rotation,           // Rotation
+        100.0,              // FOV in x
+        100.0,              // FOV in y
+        center_uv           // FIXME
+    );
+
+    let (image_tensor, _aux) = gaussian_splats::render_splats(
+        splats, 
+        &camera, 
+        img_size, 
+        background, 
+        None, 
+        gaussian_splats::TextureMode::Float,
+    ).await;
+
+    let tensor_raw = image_tensor.into_data();
+    let floats: Vec<f32> = tensor_raw.to_vec().expect("expected f32 tensor");
+
+    let proc_floats: Vec<u8> = floats.iter()
+        .map(|f| (f.clamp(0.0, 1.0) * 255.0) as u8)
+        .collect();
+
+    
+
 }
 
 /// Helper function to construct quaternions from different convention
