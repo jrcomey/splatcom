6135813e209c130ae1b7319d00df5cab0c5ee656
jrcomey <jack.rhys.comey@gmail.com>
2026-05-15 18:08:10 -0700

    Boilerplate for render

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,13 @@
 use std::path::Path;
 
 use anyhow::Result;
-use brush_render::gaussian_splats::{SplatRenderMode, Splats};
+use brush_render::gaussian_splats::{self, SplatRenderMode, Splats};
 use brush_serde::load_splat_from_ply;
 use burn::backend::wgpu::WgpuDevice;
 use burn::tensor::Device;
 use tokio::io::BufReader;
+
+use crate::message;
 
 /// AI: Load a 3D Gaussian splat scene from a `.ply` file on disk.
 ///
@@ -21,4 +23,31 @@ pub async fn load_ply_file(filepath: impl AsRef<Path>, subsample_points: Option<
     let mode = msg.meta.render_mode.unwrap_or(SplatRenderMode::Default);
 
     Ok(msg.data.into_splats(&device, mode))
+}
+
+
+
+pub fn render(request: message::ImageRequest, splats: Splats) {
+
+    // let camera = brush_render::camera::Camera::new(
+    //     position,           // Position 
+    //     rotation,           // Rotation
+    //     fov_x,              // FOV in x
+    //     fov_y,              // FOV in y
+    //     center_uv           // FIXME
+    // )
+
+    // gaussian_splats::render_splats(
+    //     splats, 
+    //     camera, 
+    //     img_size, 
+    //     background, 
+    //     splat_scale, 
+    //     texture_mode
+    // );
+
+
+
+    
+    
 }
